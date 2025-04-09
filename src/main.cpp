906713317b2b3452b2563f6c1af94a6b748c9831
DevColaDavid <devcoladavid@gmail.com>
2025-04-08 21:34:19 -1000

    added test auton

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,20 @@
 #include "main.h"
-#include "lemlib/api.hpp" // IWYU pragma: keep
 #include "robodash/views/selector.hpp"
-rd::Selector selector({
+
+// ================================= Views ================================= //
+
+rd::Selector selector({ // create robodash auton selector
     {"test auton", testAuton},
 });
 
-// initialize function. Runs on program startup
+rd::Console console; // create robodash console
+
+
+// ========================= Competition Functions ========================= //
+
 void initialize() {
-  pros::lcd::initialize(); // initialize brain screen
-  chassis.calibrate();     // calibrate sensors
-
-  pros::Task screenTask([&]() {
-    while (true) {
-      // print robot location to the brain screen
-      pros::lcd::print(0, "X: %f", chassis.getPose().x);         // x
-      pros::lcd::print(1, "Y: %f", chassis.getPose().y);         // y
-      pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-      // log position telemetry
-      lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
-      // delay to save resources
-      pros::delay(50);
-    }
-  });
-
+  pros::lcd::initialize();
+  chassis.calibrate(); // calibrate sensors
   pros::Task liftControlTask([] {
     while (true) {
       liftControl();
@@ -33,9 +25,9 @@ void initialize() {
 
 void disabled() {}
 
-void competition_initialize() {}
-
-ASSET(example_txt); // '.' replaced with "_" to make c++ happy
+void competition_initialize() {
+  selector.focus();
+}
 
 void autonomous() { selector.run_auton(); }
 
